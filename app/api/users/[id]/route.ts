d360be9269df24d529c719bf2bae0c1d9f662b89
lokyijeslynt <lokyijeslynt@student.unimelb.edu.au>
2025-10-16 12:08:53 +1100

    fixed broken routes

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -4,7 +4,7 @@
 *******************************************************/
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "@/lib/utils/auth";
 import pool from "@/lib/db";
 import bcrypt from "bcryptjs";
 import { z } from "zod";
